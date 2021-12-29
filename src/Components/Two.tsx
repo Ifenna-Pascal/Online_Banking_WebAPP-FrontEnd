f67683e86cec2da9e9ed2018deb824ced39cea71
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2021-12-29 18:13:16 +0100

    added another section

--- src/Components/Two.tsx
+++ src/Components/Two.tsx
@@ -10,7 +10,7 @@ const Twos: React.FC<IProps> = ({ img, text_p, text_h1, reverse }) => {
   return (
     <div className="h-screen sm:h-17/20 flex flex-col items-center">
       <div
-        className={`container flex flex-col  sm:mt-8 mt-6 ${
+        className={`container flex flex-col xs:mt-4 sm:mt-8 mt-6 ${
           reverse ? "lg:flex-row-reverse" : "lg:flex-row"
         } items-center justify-center gap-x-24`}
       >
@@ -24,10 +24,10 @@ const Twos: React.FC<IProps> = ({ img, text_p, text_h1, reverse }) => {
         </div>
         {/* <!-- Content --> */}
         <div className="flex py-1 px-3 flex-1 flex-col sm:gap-5 gap-3 items-center lg:items-start">
-          <h1 className="sm:text-4xl xs:text-2xl text-4xl leading-10 text-center sm:text-left font-Poppins font-bold text-indigo-500">
+          <h1 className="sm:text-4xl xs:text-3xl text-4xl leading-10 text-center sm:text-left font-Poppins font-bold text-indigo-500">
             {text_h1}
           </h1>
-          <p className="text-gray-500 font-Poppins text-lg sm:text-lg my-2 sm:my-4 text-center lg:text-left sm:w-3/4 lg:w-3/4">
+          <p className="text-gray-500 font-Poppins text-lg xs:text-md sm:text-lg my-2 sm:my-4 text-center lg:text-left sm:w-3/4 lg:w-3/4">
             {text_p}
           </p>
           <button className="text-sm flex-1 text-center sm:mt-0 mt-4">
